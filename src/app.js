997c238f67e85d29e5ab838e35bfcfdf1985a77d
alejoforeroforero <alejoforeroforero@gmail.com>
2024-09-08 06:57:27 -0500

    con cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors"); 
 require("dotenv").config();
 
 const { Pool } = require("pg");
