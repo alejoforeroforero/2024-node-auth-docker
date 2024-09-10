8200ab7ead14ea84326055b96911df30e6ff3e7c
alejoforeroforero <alejoforeroforero@gmail.com>
2024-09-09 21:25:27 -0500

    intento

--- src/app.js
+++ src/app.js
@@ -27,8 +27,10 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: [process.env.HOST1, process.env.HOST2],
-  credentials: true
+  origin: [process.env.HOST1, process.env.HOST2, 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Rutas
